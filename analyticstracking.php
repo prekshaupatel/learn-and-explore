f63fe2b086dc08cc8fe0f102243849f7ba7747b5
Preksha Patel <prekshapatel@Prekshas-MacBook-Pro.local>
2020-12-30 21:15:17 +0530

    analytics

---
+++ analyticstracking.php
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NTFR5PJF2Z"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NTFR5PJF2Z');
+</script>
