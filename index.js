265526838e481a41b16aad33bd1873dd0cd01da9
Preksha Patel <prekshapatel@Prekshas-MacBook-Pro.local>
2021-01-02 23:50:55 +0530

    hide derivation (enzyme)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function hide_steps(name, button_name) {
+function hide_steps(name, button_name, msg) {
     var x = document.getElementById(name);
     if (x.style.display === "none") {
 	x.style.display = "block";
-	document.getElementById(button_name).innerHTML = "Hide Steps";
+	document.getElementById(button_name).innerHTML = "Hide "+msg;
     } else {
 	x.style.display = "none";
-	document.getElementById(button_name).innerHTML	= "Show Steps";
+	document.getElementById(button_name).innerHTML	= "Show "+msg;
     }
 }
